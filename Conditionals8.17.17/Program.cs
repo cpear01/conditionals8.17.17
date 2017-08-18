befeb94e8855c7fb5e6630429ac77f370bb259be
cpear01 <cpearson_su@yahoo.com>
2017-08-17 20:46:09 -0400

    first commit

---
+++ Conditionals8.17.17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals8._17._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
